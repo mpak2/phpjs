345588f1c956f0141fd54ee81d4a4049654f52d0
mpak2 <github.com@mpak.su>
2018-04-26 11:08:48 +0300

    array_column

---
+++ functions/array/array_column.js
@@ -0,0 +1,20 @@
+function array_column(input, column_key, index_key){ // search_value, argStrict
+  // https://github.com/mpak2/phpjs/
+  console.log("input:", input);
+  var search = typeof search_value !== 'undefined',
+    tmp_obj = {},
+    include = true,
+    key = 0;
+
+  for(key in Object(input)){
+    if(!input.hasOwnProperty(key)){// console.error("ОШИБКА ключ `"+ key+ "` не анйден в массиве");
+    }else if("object" != typeof(input[key])){// console.log("Не верный тип элемента");
+    }else if(!input[key].hasOwnProperty(column_key)){// console.error("ОШИБКА поле в массиве не установлено");
+    }else if("undefined" == (column_value = input[key][column_key])){ console.error("ОШИБКА получения значения массива", input[key], column_key);
+		}else if(isNaN(index_value = (input[key].hasOwnProperty(index_key) ? input[key][index_key] : "undefined"))){ console.log("ОШИБКА расчета значения ключа");
+    }else{// console.log("index_key:", index_key, "index_value", index_value, "column_value:", column_value, input[key].hasOwnProperty(index_key), input[key][index_key], input[key]);
+      tmp_obj[index_value] = column_value;
+    }
+  } return tmp_obj;
+}
+
